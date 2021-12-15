5b5db25c0f2f3d80b08e6e9a457d5c22c9395423
Gilang Pratama <pratamapriadi96@gmail.com>
2021-12-16 05:13:19 +0700

    :zap: Update lint

--- components/from-head.js
+++ components/from-head.js
@@ -5,7 +5,7 @@ function FormHead() {
 				Huggingface 🤗 Transformer Summarization
 			</div>
 			<div className="mt-4 self-center text-xl sm:text-sm text-gray-800 text-center">
-				Sebuah program kecil menggunakan kekuatan artificial intelligence untuk merangkum konten berita. Simak cara kerjanya <a className="text-red-500" target="_blank" href="https://github.com/kangketikonlen/my-notebook/blob/main/Huggingface-transformer-summarization.ipynb">di sini</a> dan <a href="https://github.com/kangketikonlen/my-notebook/blob/main/Huggingface-transformer-summarization-v2.ipynb" target="_blank" className="text-red-500">di sini</a>.
+				Sebuah program kecil menggunakan kekuatan artificial intelligence untuk merangkum konten berita. Simak cara kerjanya <a className="text-red-500" target="_blank" href="https://github.com/kangketikonlen/my-notebook/blob/main/Huggingface-transformer-summarization.ipynb" rel="noreferrer" >di sini</a> dan <a href="https://github.com/kangketikonlen/my-notebook/blob/main/Huggingface-transformer-summarization-v2.ipynb" target="_blank" rel="noreferrer" className="text-red-500">di sini</a>.
 			</div>
 		</>
 	)
